6e2ef7577609df948f4906338c1cb2a7e6df7651
Quentin Ceschin <ceschin.quentin@gmail.com>
2019-12-19 18:52:06 +0100

    add a basic form to test

--- SpringApp/src/main/java/hello/HelloController.java
+++ SpringApp/src/main/java/hello/HelloController.java
@@ -1,6 +1,9 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import hello.vue.FormRepoSelector;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
@@ -8,7 +11,6 @@ public class HelloController {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return FormRepoSelector.FormVue();
 	}
-	
 }
--- SpringApp/src/main/java/hello/vue/FormRepoSelector.java
+++ SpringApp/src/main/java/hello/vue/FormRepoSelector.java
@@ -2,7 +2,30 @@ package hello.vue;
 
 public class FormRepoSelector {
 
-	public String FormVue() {
-		return "some html";
+	public static String FormVue() {
+		return "<!DOCTYPE html>\r\n" + 
+				"<html lang=\"fr\">\r\n" + 
+				"<head>\r\n" + 
+				"    <meta charset=\"UTF-8\">\r\n" + 
+				"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + 
+				"    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\r\n" + 
+				"    <title>Document</title>\r\n" + 
+				"</head>\r\n" + 
+				"<body>\r\n" + 
+				"    <form action=\"\" method=\"get\" class=\"form-repo-select\">\r\n" + 
+				"        <div class=\"form-repo-select\">\r\n" + 
+				"          <label for=\"nameRepo\">Enter the repo name: </label>\r\n" + 
+				"          <input type=\"text\" name=\"name\" id=\"name\" required>\r\n" + 
+				"        </div>\r\n" + 
+				"        <div class=\"form-repo-select\">\r\n" + 
+				"          <label for=\"email\">Enter your email: </label>\r\n" + 
+				"          <input type=\"email\" name=\"email\" id=\"email\" required>\r\n" + 
+				"        </div>\r\n" + 
+				"        <div class=\"form-repo-select\">\r\n" + 
+				"          <input type=\"submit\" value=\"Subscribe!\">\r\n" + 
+				"        </div>\r\n" + 
+				"      </form>\r\n" + 
+				"</body>\r\n" + 
+				"</html>";
 	}
 }
