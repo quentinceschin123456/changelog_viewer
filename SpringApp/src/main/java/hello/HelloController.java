dbde793b3d082944c1efa987a0be3d365cf3ec72
Ancelin GLORIES <ancelin.glories@gmail.com>
2019-12-20 14:08:01 +0100

    Test Web avec fichier JSP

--- SpringApp/src/main/java/hello/HelloController.java
+++ SpringApp/src/main/java/hello/HelloController.java
@@ -1,14 +1,16 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller // Nécéssaire de spécifier @Controller à la place de @RestController
 public class HelloController {
 	
+	//Un fichier nommé "application.properties" (situé dans src/main/ressources) est lu. C'est une configuration de Spring
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "hello"; // Grâce à "application.property", "return hello" pointe sur src/main/webapp/web-inf/jsp/hello.jsp
 	}
 	
 }
